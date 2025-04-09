c90ebe2c37067885e117983e736c79ab082da09d
Rice Lin <rice.lin@icloud.com>
2025-04-10 00:04:55 +0800

    205. Isomorphic Strings

---
+++ LeetCode.playground/Pages/205. Isomorphic Strings.xcplaygroundpage/Contents.swift
@@ -0,0 +1,61 @@
+/*:
+ # 205. Isomorphic Strings
+
+ Given two strings `s` and `t`, determine if they are isomorphic.
+
+ Two strings `s` and `t` are isomorphic if the characters in `s` can be replaced to get `t`.
+
+ All occurrences of a character must be replaced with another character while preserving the order of characters. No two characters may map to the same character, but a character may map to itself.
+
+ * Callout(Example 1):
+ Input: s = "egg", t = "add"\
+ Output: true\
+ Explanation:\
+ The strings s and t can be made identical by:
+ - Mapping 'e' to 'a'.
+ - Mapping 'g' to 'd'.
+ ---
+ * Callout(Example 2):
+ Input: s = "foo", t = "bar"\
+ Output: false\
+ Explanation:\
+ The strings s and t can not be made identical as 'o' needs to be mapped to both 'a' and 'r'.
+ ---
+ * Callout(Example 3):
+ Input: s = "paper", t = "title"\
+ Output: true
+ ---
+
+ **Constraints:**
+ - 1 <= s.length <= 5 * 10^4
+ - t.length == s.length
+ - s and t consist of any valid ascii character.
+ */
+class Solution {
+  func isIsomorphic(_ s: String, _ t: String) -> Bool {
+    var sMap = [Int](repeating: -1, count: 128)
+    var tMap = [Int](repeating: -1, count: 128)
+    
+    let sArray = Array(s)
+    let tArray = Array(t)
+    
+    for i in 0 ..< s.count {
+      let a = Int(sArray[i].asciiValue!)
+      let b = Int(tArray[i].asciiValue!)
+      
+      if sMap[a] != tMap[b] {
+        return false
+      }
+      
+      sMap[a] = i
+      tMap[b] = i
+    }
+    
+    return true
+  }
+}
+
+let s = Solution()
+print(s.isIsomorphic("egg", "add") == true)
+print(s.isIsomorphic("foo", "bar") == false)
+print(s.isIsomorphic("paper", "title") == true)
